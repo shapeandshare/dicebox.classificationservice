acf0bf76486a8d03f9495464d4cd8251ed4fae16
Joshua C. Burt <joshburt@shapeandshare.com>
2019-03-22 16:23:33 -0600

    categories are now loaded from the weights directory along with the weights added comments to code base updated response code were incorrect removed from overly broad exception handling Updated Core Libraries

--- app/classificationservice.py
+++ app/classificationservice.py
@@ -1,4 +1,15 @@
 #!flask/bin/python
+###############################################################################
+# Classification Service
+#   Handles requests for classification of data from a client.
+#
+# Copyright (c) 2017-2019 Joshua Burt
+###############################################################################
+
+
+###############################################################################
+# Dependencies
+###############################################################################
 import lib.docker_config as config
 from flask import Flask, jsonify, request, make_response, abort
 from flask_cors import CORS, cross_origin
@@ -7,7 +18,10 @@ from lib.network import Network
 import logging
 import json
 
+
+###############################################################################
 # Setup logging.
+###############################################################################
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -16,15 +30,37 @@ logging.basicConfig(
     filename="%s/classificationservice.log" % config.LOGS_DIR
 )
 
+
+###############################################################################
+# Create the network.
+###############################################################################
 network = Network(config.NN_PARAM_CHOICES)
 
-with open('./category_map.json') as data_file:
+
+###############################################################################
+# Load categories for the model
+###############################################################################
+with open('%s/category_map.json' % config.WEIGHTS_DIR) as data_file:
     jdata = json.load(data_file)
 server_category_map = {}
 for d in jdata:
     server_category_map[str(jdata[d])] = str(d)
+logging.info('loaded categories: %s' % server_category_map)
 
 
+###############################################################################
+# Create the flask, and cors config
+###############################################################################
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:*"}})
+
+
+###############################################################################
+# Method call that creates the classification response.
+# We are loading a specific model (lonestar), with a pre-created weights file.
+# Here we elect to create_model, which will also load the model weights on
+# first use.
+###############################################################################
 def get_classification(image_data):
     try:
         network.create_lonestar(create_model=True,
@@ -33,33 +69,42 @@ def get_classification(image_data):
         logging.error('Error summoning lonestar')
         return -1
 
-    try:
-        classification = network.classify('dicebox_raw', image_data)
-        logging.info("classification: (%s)" % classification)
-        return classification[0]
+    classification = network.classify(image_data)
+    logging.info("classification: (%s)" % classification)
+    return classification[0]
 
-    except:
-        logging.error('Error making prediction..')
-        return -1
+# We need more specificity before we start catching stuff here, otherwise this
+# catches too many issues.
+#
+#    try:
+#        classification = network.classify(image_data)
+#        logging.info("classification: (%s)" % classification)
+#        return classification[0]
+#    except:
+#        logging.error('Error making prediction..')
+#        return -1
 
 
-app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:*"}})
-
+###############################################################################
+# Provides categories for client consumption
+###############################################################################
 @app.route('/api/categories', methods=['GET'])
 def make_api_categorymap_public():
     if request.headers['API-ACCESS-KEY'] != config.API_ACCESS_KEY:
-        abort(401)
+        abort(403)
     if request.headers['API-VERSION'] != config.API_VERSION:
         abort(400)
 
-    return make_response(jsonify({'category_map': server_category_map}), 201)
+    return make_response(jsonify({'category_map': server_category_map}), 200)
 
 
+###############################################################################
+# Endpoint which provides classification of image data.
+###############################################################################
 @app.route('/api/classify', methods=['POST'])
 def make_api_get_classify_public():
     if request.headers['API-ACCESS-KEY'] != config.API_ACCESS_KEY:
-        abort(401)
+        abort(403)
     if request.headers['API-VERSION'] != config.API_VERSION:
         abort(400)
     if not request.json:
@@ -71,25 +116,37 @@ def make_api_get_classify_public():
     decoded_image_data = base64.b64decode(encoded_image_data)
     classification = get_classification(decoded_image_data)
 
-    return make_response(jsonify({'classification': classification}), 201)
+    return make_response(jsonify({'classification': classification}), 200)
 
 
+###############################################################################
+# Returns API version
+###############################################################################
 @app.route('/api/version', methods=['GET'])
 def make_api_version_public():
-    return make_response(jsonify({'version':  str(config.API_VERSION)}), 201)
+    return make_response(jsonify({'version':  str(config.API_VERSION)}), 200)
 
 
+###############################################################################
+# Super generic health end-point
+###############################################################################
 @app.route('/health/plain', methods=['GET'])
 @cross_origin()
 def make_health_plain_public():
-    return make_response('true', 201)
+    return make_response('true', 200)
 
 
+###############################################################################
+# 404 Handler
+###############################################################################
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
+###############################################################################
+# main entry point, thread safe
+###############################################################################
 if __name__ == '__main__':
     logging.debug('starting flask app')
     app.run(debug=config.FLASK_DEBUG, host=config.LISTENING_HOST, threaded=True)
